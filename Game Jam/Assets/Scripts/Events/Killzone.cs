3a1cd59c7fa03e7aee0a9c2a47db02ef890f2ccc
hapu <hapu.x2@gmail.com>
2021-01-31 17:26:40 -0600

    last min changes

--- Game Jam/Assets/Scripts/Events/Killzone.cs
+++ Game Jam/Assets/Scripts/Events/Killzone.cs
@@ -1,26 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Killzone : MonoBehaviour
 {
-
-    
-
-    //private float fullDamage = 1000f; // comented while it isn't used
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
