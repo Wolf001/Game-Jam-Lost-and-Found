71bc025e5692f5ab83134f9419fad123ae4b0509
hapu <hapu.x2@gmail.com>
2021-01-30 22:36:29 -0600

    clean and add script for patroling

---
+++ Game Jam/Assets/Scripts/Enemy_Ai/EnemyBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    private AIDestinationSetter following;
+    private Patrol patrolling;
+    public float threatRange = 3.5f;
+
+    private void Start()
+    {
+        following = GetComponent<AIDestinationSetter>();
+        patrolling = GetComponent<Patrol>();
+    }
+
+    private void FixedUpdate()
+    {
+        float temp_distance = (following.target.position - transform.position).magnitude;
+        if (temp_distance > threatRange)
+        {
+            patrolling.enabled = true;
+            following.enabled = false;
+        }
+        else
+        {
+            patrolling.enabled = false;
+            following.enabled = true;
+        }
+    }
+}
