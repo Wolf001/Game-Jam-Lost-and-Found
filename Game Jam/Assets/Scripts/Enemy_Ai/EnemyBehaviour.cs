fe1abe07bcb6ae8b171054db952ec5d1622cfd79
hapu <hapu.x2@gmail.com>
2021-01-31 02:58:46 -0600

    add direction to enemys

--- Game Jam/Assets/Scripts/Enemy_Ai/EnemyBehaviour.cs
+++ Game Jam/Assets/Scripts/Enemy_Ai/EnemyBehaviour.cs
@@ -5,12 +5,14 @@ using Pathfinding;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    private AIPath aiPath;
     private AIDestinationSetter following;
     private Patrol patrolling;
     public float threatRange = 3.5f;
 
     private void Start()
     {
+        aiPath = GetComponent<AIPath>();
         following = GetComponent<AIDestinationSetter>();
         patrolling = GetComponent<Patrol>();
     }
@@ -27,6 +29,14 @@ public class EnemyBehaviour : MonoBehaviour
         {
             patrolling.enabled = false;
             following.enabled = true;
+        }
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if(aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
         }
     }
 }
